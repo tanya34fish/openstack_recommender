dae0217264429b97c8ccc6c7b626f8a94ddff2b4
Fish Wang <tanya34fish@gmail.com>
2016-03-19 13:23:54 +0000

    update predict.py

--- predict.py
+++ predict.py
@@ -13,6 +13,7 @@ def predict(modelname, userList):
 		for user in userList:
 			outRowList = recommendedItemList[recommendedItemList['user_id']==user]
 			resultList[user] = list(outRowList['item_id'])
+		print resultList
 		return json.dumps(resultList)
 
 	else:
@@ -21,27 +22,23 @@ def predict(modelname, userList):
 
 if __name__ == "__main__":
 	#TODO how to get requested user ?
-	dataname = sys.argv[1]
-	filename, file_extension = os.path.splitext(dataname)
+	modelname = sys.argv[1]
+	filename, file_extension = os.path.splitext(modelname)
+	fileList = filename.split('_')
+
 	jsonfile = sys.argv[2]
-	modeltype = 'default'
-
-	if len(sys.argv) == 4:
-		modeltype = sys.argv[3]
-
-	modelname = filename + '_' + modeltype + '.model'
 	zipname = modelname + '.zip'
 
 	download("models", zipname)
+	
 	zip_ref = zipfile.ZipFile(zipname, 'r')
-	zip_ref.extractall('.')
+	zip_ref.extractall(modelname)
 	zip_ref.close()
 
 	# predict
 	# reqUserList = {"userList": ["A3SGXH7AUHU8GW"]}
 	with open(jsonfile) as data_file:
 		userDict = json.load(data_file)
-		
-	queryList = [str(v) for v in userDict['userList']]
+	#queryList = [str(v) for v in userDict['userList']]
 	resultList = predict(modelname, userDict['userList'])
 
